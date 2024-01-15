f651f67f5efb155b086645df29eb58ec01ff59d2
dominique39 <440000323@gapps.yrdsb.ca>
2024-01-15 16:36:16 -0500

    Added button to main screen in menu world

--- final-project/Menu.java
+++ final-project/Menu.java
@@ -3,7 +3,7 @@ import greenfoot.*;
 public class Menu extends WorldwCursor
 {
     private int onLevel;
-    private Button[] buttons = new Button[2];
+    private Button[] buttons = new Button[3];
     private Label menuLabel;
 
     public Menu(int lv){    
@@ -14,6 +14,7 @@ public class Menu extends WorldwCursor
         
         buttons[0] = new Button(restart,"restart");
         buttons[1] = new Button(exit,"exit_level");
+        buttons[2] = new Button(quit,"back_main_screen");
 
         for(int i = 0; i < buttons.length; i++){
             addObject(buttons[i], getWidth()/2, 100+(getHeight()-200)/(buttons.length+2)*(i+2));
@@ -22,15 +23,19 @@ public class Menu extends WorldwCursor
 
     private void toWorld(String world){
         switch(world){
-            case "restart":
+            case "Level":
                 Greenfoot.setWorld(new Transition(onLevel));
                 break;
-            case "exit":
+            case "LvSelection":
                 Greenfoot.setWorld(new LvSelection());
+                break;
+            case "MainScrn":
+                Greenfoot.setWorld(new MainScrn());
                 break;
         }
     }
 
-    private Clickable restart = () -> toWorld("restart");
-    private Clickable exit = () -> toWorld("exit");
+    private Clickable restart = () -> toWorld("Level");
+    private Clickable exit = () -> toWorld("LvSelection");
+    private Clickable quit = () -> toWorld("MainScrn");
 }
